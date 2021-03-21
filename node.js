3c358bab033b6317177903bb86c44a4f619310aa
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2021-03-21 13:05:07 +0300

    Add shared, node configs

---
+++ node.js
@@ -0,0 +1,40 @@
+const shared = require('./shared')
+
+module.exports = {
+  extends: [
+    'airbnb-base',
+    shared
+  ],
+  plugins: ['@babel'],
+  rules: {
+    'new-cap': 'off',
+    'no-invalid-this': 'off',
+    'no-unused-expressions': 'off',
+    'object-curly-spacing': 'off',
+    semi: 'off',
+    '@babel/new-cap': ['error', {
+      newIsCap: true,
+      newIsCapExceptions: [],
+      capIsNew: false,
+      capIsNewExceptions: ['Immutable.Map', 'Immutable.Set', 'Immutable.List']
+    }],
+    '@babel/no-invalid-this': 'off',
+    '@babel/no-unused-expressions': ['error', {
+      allowShortCircuit: false,
+      allowTernary: false,
+      allowTaggedTemplates: false
+    }],
+    '@babel/object-curly-spacing': ['error', 'always'],
+    '@babel/semi': ['error', 'never'],
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        '@babel/plugin-syntax-class-properties'
+      ]
+    }
+  }
+}
---
+++ shared.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    'sort-destructure-keys'
+  ],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'object-curly-newline': ['error', { multiline: true, consistent: true }],
+    'sort-imports': ['error', { ignoreDeclarationSort: true }],
+    'import/order': ['error', { groups: ['builtin', 'external', 'internal'], alphabetize: { order: 'asc' } }],
+    'sort-destructure-keys/sort-destructure-keys': 'error'
+  }
+}
