bc906ad16d39a0b00310fd642099cd4e89b038c9
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-10-31 05:08:39 +0300

    Add node typescript config export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import browser from './configs/browser.js'
 import node from './configs/node.js'
+import nodeTypescript from './configs/node-typescript.js'
 
 export default {
   browser,
-  node
+  node,
+  nodeTypescript
 }
