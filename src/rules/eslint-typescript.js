d2cc7671e9eade3e97f66773031327dabb54488c
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-17 10:22:02 +0300

    Turn off no-non-null-assertion

--- src/rules/eslint-typescript.js
+++ src/rules/eslint-typescript.js
@@ -71,7 +71,7 @@ export default {
     '@typescript-eslint/no-namespace': 'error',
     '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
     '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-redeclare': 'off',
     '@typescript-eslint/no-redundant-type-constituents': 'error',
     '@typescript-eslint/no-require-imports': 'error',
