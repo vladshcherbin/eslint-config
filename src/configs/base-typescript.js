28f31b0556afe6c80e440c99efe28206d2806d65
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-21 05:52:28 +0300

    Re-use base config

--- src/configs/base-typescript.js
+++ src/configs/base-typescript.js
@@ -1,18 +1,11 @@
 import eslintTypescript from '../rules/eslint-typescript.js'
-import eslint from '../rules/eslint.js'
-import ignores from '../rules/ignores.js'
 import importX from '../rules/import-x.js'
 import perfectionist from '../rules/perfectionist.js'
-import sortDestructureKeys from '../rules/sort-destructure-keys.js'
-import stylistic from '../rules/stylistic.js'
+import base from './base.js'
 
 export default [
-  eslint,
+  ...base,
   eslintTypescript,
-  importX.base,
   importX.typescript,
-  stylistic,
-  sortDestructureKeys,
-  perfectionist.base,
-  ignores
+  perfectionist.typescript
 ]
