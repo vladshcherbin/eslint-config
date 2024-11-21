9c3c785255ff8ac752e7aeadfa06b1e06911b810
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-21 18:00:50 +0300

    Add react-hooks

--- src/configs/react.js
+++ src/configs/react.js
@@ -1,3 +1,4 @@
+import reactHooks from 'eslint-plugin-react-hooks'
 import importX from '../rules/import-x.js'
 import perfectionist from '../rules/perfectionist.js'
 import react from '../rules/react.js'
@@ -6,6 +7,7 @@ import browser from './browser.js'
 export default [
   ...browser,
   react,
+  reactHooks.configs.recommended,
   importX.react,
   perfectionist.react
 ]
