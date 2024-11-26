5d22659e9554bf55d2712eb0d12c10ac377db744
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-26 08:41:13 +0300

    Remove sort-destructure-keys plugin

--- src/configs/base.js
+++ src/configs/base.js
@@ -2,14 +2,12 @@ import eslint from '../rules/eslint.js'
 import ignores from '../rules/ignores.js'
 import importX from '../rules/import-x.js'
 import perfectionist from '../rules/perfectionist.js'
-import sortDestructureKeys from '../rules/sort-destructure-keys.js'
 import stylistic from '../rules/stylistic.js'
 
 export default [
   eslint,
   importX.base,
   stylistic.base,
-  sortDestructureKeys,
   perfectionist.base,
   ignores
 ]
