2dc049a0b3e5e47e7d9ddc9fe5435310fa4a58d7
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-12-06 02:00:56 +0300

    Add next config export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import browser from './configs/browser.js'
+import next from './configs/next.js'
 import nodeTypescript from './configs/node-typescript.js'
 import node from './configs/node.js'
 import react from './configs/react.js'
 
 export default {
   browser,
+  next,
   node,
   nodeTypescript,
   react
