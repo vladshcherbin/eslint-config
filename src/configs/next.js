87f753f7b5f2d8bbb5b887c9d4f284375dfee008
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-12-12 07:48:54 +0300

    Add node packages support to next config

--- src/configs/next.js
+++ src/configs/next.js
@@ -1,7 +1,32 @@
+import confusingBrowserGlobals from 'confusing-browser-globals'
+import globals from 'globals'
+import importX from '../rules/import-x.js'
+import jsxA11y from '../rules/jsx-a11y.js'
 import next from '../rules/next.js'
-import react from './react.js'
+import perfectionist from '../rules/perfectionist.js'
+import reactHooks from '../rules/react-hooks.js'
+import react from '../rules/react.js'
+import stylistic from '../rules/stylistic.js'
+import base from './base.js'
 
 export default [
-  ...react,
-  next
+  ...base,
+  react,
+  reactHooks,
+  jsxA11y,
+  stylistic.jsx,
+  importX.react,
+  perfectionist.react,
+  next,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.nodeBuiltin
+      }
+    },
+    rules: {
+      'no-restricted-globals': ['error', 'isFinite', 'isNaN', ...confusingBrowserGlobals]
+    }
+  }
 ]
