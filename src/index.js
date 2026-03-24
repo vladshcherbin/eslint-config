884494cdfba9b2cc8c1054eb665de45f179f1095
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2026-03-24 03:19:04 +0300

    Remove import plugin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'eslint/config'
 import eslint from './rules/eslint.js'
 import ignores from './rules/ignores.js'
-import importX from './rules/import-x.js'
 import perfectionist from './rules/perfectionist.js'
 import stylistic from './rules/stylistic.js'
 import typescript from './rules/typescript.js'
@@ -12,7 +11,6 @@ export default defineConfig([
     extends: [
       eslint,
       typescript,
-      importX,
       stylistic,
       perfectionist
     ]
